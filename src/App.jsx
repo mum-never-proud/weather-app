c5ec1d35b95dcdc3c119380512d612d7455591d5
mum-never-proud <abhuz@hotmail.com>
2020-10-11 14:07:13 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import {
+  BrowserRouter as Router, Route, Redirect, Switch,
+} from 'react-router-dom';
 import React from 'react';
+import Forecast from '@components/Forecast';
+import Footer from '@common/Footer';
+import Header from '@common/Header';
+import Home from '@components/Home';
+import '@/App.css';
 
 const App = () => (
-  <div>Weather App</div>
+  <Router>
+    <Header />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/forecast/:place_id" component={Forecast} />
+      <Route path="*">
+        <Redirect to="/" />
+      </Route>
+    </Switch>
+    <Footer />
+  </Router>
 );
 
 export default App;
