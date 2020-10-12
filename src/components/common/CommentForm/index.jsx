e20d1339d116062b5d3644d601ba0543e0278a7f
mum-never-proud <abhuz@hotmail.com>
2020-10-12 09:56:26 +0530

    comment-form

--- src/components/common/CommentForm/index.jsx
+++ src/components/common/CommentForm/index.jsx
@@ -6,6 +6,9 @@ const CommentForm = () => {
   const commentRef = useRef();
   const [charactersLeft, setCharactersLeft] = useState(MAX_CHARACTERS_ALLOWED);
   const [comment, setComment] = useState('');
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
 
   useEffect(() => {
     commentRef.current.focus();
@@ -16,7 +19,7 @@ const CommentForm = () => {
   }, [comment]);
 
   return (
-    <form className="weather-card--form mt-2">
+    <form className="weather-card--form mt-2" onSubmit={submitHandler}>
       <textarea
         className="form-control resize-none p-1"
         value={comment}
@@ -24,12 +27,15 @@ const CommentForm = () => {
         ref={commentRef}
         onChange={(e) => setComment(e.target.value)}
       />
-      <div className="mt-1">
-        <small>
-          <span className="text-mute">Characters Left</span>
-          {' '}
-          <span className={`font-weight-bold ${charactersLeft < 0 ? 'text-danger' : ''}`}>{charactersLeft}</span>
-        </small>
+      <div className="mt-1 d-flex justify-content-between">
+        <div>
+          <small>
+            <span className="text-mute">Characters Left</span>
+            {' '}
+            <span className={`font-weight-bold ${charactersLeft < 0 ? 'text-danger' : ''}`}>{charactersLeft}</span>
+          </small>
+        </div>
+        <button type="submit" className="primary">Submit</button>
       </div>
     </form>
   );
